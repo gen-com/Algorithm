7b67361fb2e38371d03c075089255c352b074439
Koo <31850425+gen-com@users.noreply.github.com>
2022-05-25 18:07:40 +0900

    [Silver I] Title: IOIOI, Time: 72 ms, Memory: 99060 KB -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+// MARK: - Thanks to Wapas
+
+final class FileIO {
+    
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1 : -1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex ..< (index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readStirngSum() -> Int {
+        var byte = read()
+        while byte == 10 || byte == 32 { byte = read() }
+        var sum = Int(byte)
+        while byte != 10 && byte != 32 && byte != 0 { byte = read(); sum += Int(byte) }
+        
+        return sum - Int(byte)
+    }
+}
+
+// MARK: Input
+
+let fileIO = FileIO()
+let n = fileIO.readInt()
+let m = fileIO.readInt()
+let s = fileIO.readString().map { String($0) }
+
+var current = 0
+var answer = 0
+while current < m {
+    var count = 0
+    if s[current] == "I" {
+        while current + 2 < m, s[current + 1] == "O", s[current + 2] == "I" {
+            current += 2
+            count += 1
+            if count == n {
+                answer += 1
+                count -= 1
+            }
+        }
+    }
+    current += 1
+}
+print(answer)
