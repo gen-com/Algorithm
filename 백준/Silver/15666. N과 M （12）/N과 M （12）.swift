ba6e6ef4bc4b0759171b650998a306fcd8f6fe9c
Koo <31850425+gen-com@users.noreply.github.com>
2022-06-07 18:46:32 +0900

    [Silver II] Title: N과 M (12), Time: 24 ms, Memory: 79792 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+// MARK: - Thanks to Wapas
+
+final class FileIO {
+    
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1 : -1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex ..< (index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readStirngSum() -> Int {
+        var byte = read()
+        while byte == 10 || byte == 32 { byte = read() }
+        var sum = Int(byte)
+        while byte != 10 && byte != 32 && byte != 0 { byte = read(); sum += Int(byte) }
+        
+        return sum - Int(byte)
+    }
+}
+
+// MARK: - Input
+
+let fileIO = FileIO()
+let n = fileIO.readInt()
+let m = fileIO.readInt()
+var array = Array(repeating: 0, count: n)
+for index in 0 ..< n {
+    array[index] = fileIO.readInt()
+}
+array.sort()
+
+// MARK: - Solution
+
+var answer = ""
+var visitedResult: [String: Bool] = [:]
+func backTracking(current: [String], nextIndex: Int) {
+    if current.count == m {
+        let result = current.reduce("") { $0 + "\($1) " }
+        if visitedResult[result] == nil {
+            visitedResult[result] = true
+            answer += "\(result)\n"
+        }
+        
+        return
+    }
+    for index in nextIndex ..< array.endIndex {
+        var next = current
+        next.append("\(array[index])")
+        backTracking(current: next, nextIndex: index)
+    }
+}
+backTracking(current: [], nextIndex: 0)
+print(answer)
