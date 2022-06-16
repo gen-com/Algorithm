44cba8c0eb1584aa20bb8719cd935f5664fd8291
Koo <31850425+gen-com@users.noreply.github.com>
2022-06-16 14:45:23 +0900

    [Silver III] Title: 1로 만들기, Time: 60 ms, Memory: 87324 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,79 @@
+import Foundation
+
+// MARK: - Thanks to Wapas
+
+final class FileIO {
+    
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        while now == 10 || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1 : -1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 { now = read() }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex ..< (index-1)]), encoding: .ascii)!
+    }
+    
+    @inline(__always) func readStirngSum() -> Int {
+        var byte = read()
+        while byte == 10 || byte == 32 { byte = read() }
+        var sum = Int(byte)
+        while byte != 10 && byte != 32 && byte != 0 { byte = read(); sum += Int(byte) }
+        
+        return sum - Int(byte)
+    }
+}
+
+// MARK: - Input
+
+let fileIO = FileIO()
+let targetNumber = fileIO.readInt()
+
+// MARK: - Solution
+
+var dp = Array(repeating: 0, count: targetNumber + 1)
+
+func solution(_ number: Int) -> Int {
+    if number < 2 {
+        dp[number] = 0
+    } else {
+        for num in 2 ... number {
+            dp[num] = dp[num - 1] + 1
+            if num % 2 == 0 {
+                dp[num] = min(dp[num], dp[num / 2] + 1)
+            }
+            if num % 3 == 0 {
+                dp[num] = min(dp[num], dp[num / 3] + 1)
+            }
+        }
+    }
+    
+    return dp[number]
+}
+print(solution(targetNumber))
